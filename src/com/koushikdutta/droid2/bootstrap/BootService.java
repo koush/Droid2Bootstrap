eba94bf2b6e19311c43ef6576dcc63372a0883a6
cvpcs <root@cvpcs.org>
2011-01-03 14:27:40 -0600

    restore d2 source files
    restoration includes addition of configuration parameters to allow
    ROMs to define which features the bootstrapper should or shouldn't
    implement

---
+++ src/com/koushikdutta/droid2/bootstrap/BootService.java
@@ -0,0 +1,67 @@
+package com.koushikdutta.droid2.bootstrap;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+
+public class BootService extends Service {
+	
+	private static final String TAG = "D2B/BootService";
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+        
+        // wait to make sure the system is sane.
+        mHandler.postDelayed(new Runnable()
+        {
+            @Override
+            public void run() {
+                
+                String filesDir = getFilesDir().getAbsolutePath();
+                String busybox = filesDir + "/busybox";
+                String adbd = filesDir + "/adbd";
+                
+                StringBuilder command = new StringBuilder();
+                
+                // prevent recovery from booting here
+                Log.d(TAG, "Removing recovery_mode trigger");
+                command.append("rm /data/.recovery_mode ; ");
+                
+                ROMBootstrapSettings settings = new ROMBootstrapSettings();
+                
+                if(settings.restartAdb()) {
+                    // restart adbd as root
+                	Log.d(TAG, "Restarting ADB as Root");
+                	command.append(busybox + " mount -orw,remount / ; ");
+                	command.append("mv /sbin/adbd /sbin/adbd.old ; ");
+                	command.append(busybox + " cp " +  adbd + " /sbin/adbd ; ");
+                	command.append(busybox + " mount -oro,remount / ; ");
+                	command.append(busybox + " kill $(ps | " + busybox + " grep adbd) ;");
+                }
+                
+                try {
+                    Helper.runSuCommand(BootService.this, command.toString());
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    stopSelf();
+                }
+            }
+        },
+        30000);
+    }
+    
+    Handler mHandler = new Handler();
+    
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+}
---
+++ src/com/koushikdutta/droid2/bootstrap/ROMBootstrapSettings.java
@@ -0,0 +1,67 @@
+package com.koushikdutta.droid2.bootstrap;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+public class ROMBootstrapSettings {
+	private static final String TAG = "D2B/ROMBootstrapSettings";
+	
+	private static final File SETTINGS_FILE = new File("/system/etc/Droid2Bootstrap.cfg");
+	private static final String RESTART_ADB_KEY = "restart_adb";
+	private static final String INSTALL_HIJACK_KEY = "install_hijack";
+	private static final String INSTALL_RECOVERY_KEY = "install_recovery";
+	
+	private boolean mRestartAdb = true;
+	private boolean mInstallHijack = true;
+	private boolean mInstallRecovery = true;
+	
+	public ROMBootstrapSettings() {
+		if(SETTINGS_FILE.exists()) {
+			Log.d(TAG, "Found settings file, parsing");
+			
+			FileInputStream f = null;
+			String data = "";
+			
+			try {
+				// read file into a string
+			    byte[] buffer = new byte[(int) SETTINGS_FILE.length()];
+				f = new FileInputStream(SETTINGS_FILE);
+				f.read(buffer);
+				data = new String(buffer);
+			} catch(Exception e) {
+				Log.e(TAG, "Error reading settings file", e);
+			} finally {
+				// ensure the stream is closed
+				if(f != null) try { f.close(); } catch(Exception ignored) { }
+			}
+			
+			try {
+				// parse as JSON
+				JSONObject json = new JSONObject(data);
+				
+				if(json.has(RESTART_ADB_KEY)) {
+					mRestartAdb = json.optBoolean(RESTART_ADB_KEY, true);
+					Log.d(TAG, "Setting [RestartAdb] to [" + (mRestartAdb ? "true" : "false") + "]");
+				}
+				if(json.has(INSTALL_HIJACK_KEY)) {
+					mInstallHijack = json.optBoolean(INSTALL_HIJACK_KEY, true);
+					Log.d(TAG, "Setting [InstallHijack] to [" + (mInstallHijack ? "true" : "false") + "]");
+				}
+				if(json.has(INSTALL_RECOVERY_KEY)) {
+					mInstallRecovery = json.optBoolean(INSTALL_RECOVERY_KEY, true);
+					Log.d(TAG, "Setting [InstallRecovery] to [" + (mInstallRecovery ? "true" : "false") + "]");
+				}
+			} catch(Exception e) {
+				Log.e(TAG, "Error parsing settings file", e);
+			}
+		}
+	}
+	
+	public boolean restartAdb() { return mRestartAdb; }
+	public boolean installHijack() { return mInstallHijack; }
+	public boolean installRecovery() { return mInstallRecovery; }
+}
