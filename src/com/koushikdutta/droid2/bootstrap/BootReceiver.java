f724aec3a2aac4268e55fcd012a238c147c8ae92
cvpcs <root@cvpcs.org>
2011-01-04 04:27:40 +0800

    restore d2 source files
    restoration includes addition of configuration parameters to allow
    ROMs to define which features the bootstrapper should or shouldn't
    implement

---
+++ src/com/koushikdutta/droid2/bootstrap/BootReceiver.java
@@ -0,0 +1,14 @@
+package com.koushikdutta.droid2.bootstrap;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, BootService.class);
+        context.startService(i);    }
+
+}
